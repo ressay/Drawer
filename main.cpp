a1cf0dbf5adc5c15c940303c9968c1cf4568914d
Wissben <wissam.benhaddad.pro@gmail.com>
2018-11-27 22:35:27 +0100

    Enhancing UI

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <QtWidgets/QApplication>
+#include <QtCore/QFile>
 #include "DetectingMethods/DetectionPolicy.h"
 
 #include "Useful/BasicMethods.h"
@@ -10,10 +11,28 @@
 using namespace cv;
 using namespace std;
 
+QByteArray readTextFile(const QString &file_path) {
+    QFile input_file(file_path);
+    QByteArray input_data;
+
+    if (input_file.open(QIODevice::Text | QIODevice::Unbuffered | QIODevice::ReadOnly)) {
+        input_data = input_file.readAll();
+        input_file.close();
+        return input_data;
+    }
+    else {
+        return QByteArray();
+    }
+}
+
+
+
 int main(int argc, char *argv[])
 {
 
     QApplication a(argc, argv);
+    QString style_sheet = readTextFile("/home/weiss/CODES/TP-VISION/Drawer/style.qss");
+    a.setStyleSheet(style_sheet);
 	testQT w;
 	w.show();
 
