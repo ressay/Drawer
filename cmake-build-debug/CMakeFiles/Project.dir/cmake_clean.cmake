dbc7e9b2e6f6abee2b203eb472a40742061eb121
ressay <bourahla.yasser@gmail.com>
2018-12-02 12:45:06 +0100

    fixed last figure

---
+++ cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_autogen.dir//AutogenOldSettings.cmake"
+  "Project_autogen/"
+  "main.moc"
+  "CMakeFiles/Project.dir/main.cpp.o"
+  "CMakeFiles/Project.dir/Useful/BasicMethods.cpp.o"
+  "CMakeFiles/Project.dir/Useful/Displayer.cpp.o"
+  "CMakeFiles/Project.dir/DetectingMethods/DetectionPolicy.cpp.o"
+  "CMakeFiles/Project.dir/testqt.cpp.o"
+  "CMakeFiles/Project.dir/qrc_testqt.cpp.o"
+  "CMakeFiles/Project.dir/Useful/KeyHandler.cpp.o"
+  "CMakeFiles/Project.dir/Drawing/Drawer.cpp.o"
+  "CMakeFiles/Project.dir/Drawing/DrawManager.cpp.o"
+  "CMakeFiles/Project.dir/Useful/Clusterer.cpp.o"
+  "CMakeFiles/Project.dir/Project_autogen/moc_compilation.cpp.o"
+  "Project.pdb"
+  "Project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
